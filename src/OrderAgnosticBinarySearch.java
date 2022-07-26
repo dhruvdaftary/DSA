6c91d7025bd5b2077a930fbd6f7a8b3ca9123371
Dhruv <dhruvdaftary@gmail.com>
2022-07-27 00:15:35 +0530

    Fixed bugs

--- src/OrderAgnosticBinarySearch.java
+++ src/OrderAgnosticBinarySearch.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BinarySearchAlgorithm {
-//    For only ascending arrays
+public class OrderAgnosticBinarySearch {
+//    For both ascending and descending arrays
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the array in sorted form");
@@ -18,10 +18,16 @@ public class BinarySearchAlgorithm {
         int target = sc.nextInt();
         System.out.println("The entered array is");
         System.out.println(Arrays.toString(arr));
-        System.out.println("The target element is present at index "+BinarySearch(arr,target));
+        if(arr[0]>arr[arr.length-1]){
+            System.out.println("The target element is present at index " + BinarySearchDescending(arr,target));
+
+        } else if ((arr[0]<arr[arr.length-1])) {
+            System.out.println("The target element is present at index " + BinarySearchAscending(arr,target));
+        }
+
     }
 
-    static int BinarySearch(int[] arr, int target){
+    static int BinarySearchAscending(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
         while (start<=end){
@@ -31,9 +37,28 @@ public class BinarySearchAlgorithm {
                 return size;
             }
             else if(arr[size]>target){
-                end = size;
+                end = size-1;
             }
             else if(arr[size/2]<target){
+                start = size+1;
+            }
+        }
+        return -1;
+    }
+
+    static int BinarySearchDescending(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+//            size = start+((end-start)/2);
+            int size = (start+end)/2;
+            if(arr[size] == target){
+                return size;
+            }
+            else if(arr[size]<target){
+                end = size;
+            }
+            else if(arr[size/2]>target){
                 start = size;
             }
         }
